8611bfb3708baeddfbf87b95489558642b9d9ee2
Ray-Xavier-2021 <horatioxhanley@gmail.com>
2023-12-28 04:34:03 -0500

    feature: Made carousel automatic -  Implement use of button

--- js/main.js
+++ js/main.js
@@ -1,70 +1,74 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-// document.querySelector('button').addEventListener('click', async () => {
-//   const MY_COCKTAIL_KEY = '1'
-//   const inputVal = document.querySelector("input").value;
-//   const COCKTAIL_URL = `https://www.thecocktaildb.com/api/json/v1/${MY_COCKTAIL_KEY}/search.php?s=${inputVal}`
-
-//   try {
-//     const cocktailResponse = await fetch(COCKTAIL_URL)
-//     if (!cocktailResponse.ok) throw new Error(`HTTP error! status: ${cocktailResponse.status}`)
-
-//     const cocktailImgData = await cocktailResponse.json()
-
-//     document.querySelector('img').src = cocktailImgData.drinks[0].strDrinkThumb
-//     document.querySelector('h2').innerText = cocktailImgData.drinks[0].strDrink
-//     document.querySelector('h3').innerText = cocktailImgData.drinks[0].strInstructions
-//   } catch (error) {
-//     console.log('Caught an error!', error.message)
-//   }
-// })
-
 // Cocktail Carousel
-document.querySelector('button').addEventListener('click', getCocktails);
+// Add an event listener to call getCocktails when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', () => {
+    getCocktails();
+  });
+
+  getCocktailsOnLoad();
+});
+
+function getCocktailsOnLoad() {
+  const input = document.querySelector('input');
+  const cocktail = input.value.trim();
+
+  if (cocktail === '') {
+    getCocktails();
+  } else {
+    // If the button is clicked later, this will set the value for fetching
+    input.value = cocktail;
+  }
+};
 
 function getCocktails() {
-  let cocktail = document.querySelector('input').value;
-  
-  fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktail)
+  let cocktail = document.querySelector('input').value.trim();
+
+  if (hasWhiteSpace(cocktail)) {
+    cocktail = cocktail.split(' ').join('%20');
+  }
+
+  fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
     .then(res => res.json())
     .then(data => {
       const cocktails = data.drinks;
-      displayCocktails(cocktails)
-      console.log(cocktails)
+      displayCocktails(cocktails);
     })
-    .catch(err => console.log(err))
-
-  
+    .catch(err => console.log(err));
 };
 
-// Display cocktails
 function displayCocktails(arr) {
   const carouselSlide = document.querySelector('.carousel-slide');
-  arr.forEach(drink => {
-    // Each drink 
-    const cocktailItem = document.createElement('div')
-    cocktailItem.className = 'carousel-item';
-    // Name of drink
-    const cocktailName = document.createElement('h2')
-    cocktailName.textContent = drink.strDrink;
-    // Image of Drink
-    const cocktailImg = document.createElement('img');
-    cocktailImg.alt =  drink.strDrink;
-    cocktailImg.src =  drink.strDrinkThumb;
+  // Clear previous items
+  carouselSlide.innerHTML = '';
 
-    // Instruction of drink
+  arr.forEach(drink => {
+    const cocktailItem = document.createElement('div');
+    cocktailItem.className = 'carousel-item';
+
+    const cocktailName = document.createElement('h2');
+    cocktailName.textContent = drink.strDrink;
+
+    const cocktailImg = document.createElement('img');
+    cocktailImg.alt = drink.strDrink;
+    cocktailImg.src = drink.strDrinkThumb;
+
     const cocktailInstructions = document.createElement('h3');
     cocktailInstructions.textContent = drink.strInstructions;
-    
-    // Add item
+
     cocktailItem.appendChild(cocktailName);
     cocktailItem.appendChild(cocktailImg);
     cocktailItem.appendChild(cocktailInstructions);
 
-    // Add item to carousel
     carouselSlide.appendChild(cocktailItem);
-      
-    console.log(drink, cocktailItem, cocktailName, cocktailImg)
-  })
+  });
+
   document.querySelector('input').value = '';
-}
+};
+
+function hasWhiteSpace(str) {
+  return /\s/g.test(str);
+};
